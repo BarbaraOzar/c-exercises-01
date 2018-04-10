382edb2a3b1e6022f0411984a4590958d92d74c4
BarbaraOzar <barbara.ozar@gmail.com>
2018-04-10 11:04:01 +0200

    exe 4
    sequence of leds lights up

--- HelloWorld/GccApplication1/main.c
+++ HelloWorld/GccApplication1/main.c
@@ -1,26 +1,33 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 2018-04-10 10:51:48
+ * Author : Barbara
+ */ 
 
 #define F_CPU 10000000L
 #include <avr/io.h>
 #include <util/delay.h>
+int led = 0;
 
 void led_on(int led_no) ;
 void led_off(int led_no);
 
 int main(void)
 {
-    DDRA = 0xff;				// setting all I/O's in port A as outputs
+	DDRA = 0xff;				// setting all I/O's in port A as outputs
 	PORTA = 0xff;				// turning all leds off
 	
-	while(1) 
+	while(led != 8)
 	{
-		led_on(3);
-		_delay_ms(500);
-		led_off(3);
-		_delay_ms(500);
-	}	
+		led_on(led);
+		_delay_ms(1000);
+		led_off(led);
+		led++;
+	}
 }
 
-void led_on(int led_no) 
+void led_on(int led_no)
 {
 	PORTA &= ~(1<<led_no);      // turning on led
 }
